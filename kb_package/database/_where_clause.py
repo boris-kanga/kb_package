483e749de709eabca3f8acd37a9fcd05ea149a68
boris-kanga <kangaborisparfait@gmail.com>
2022-12-17 00:45:45 +0000

    FEAT: add _where_clause.py module. It's use ast for parsing where clause

---
+++ kb_package/database/_where_clause.py
@@ -0,0 +1,182 @@
+import ast
+import re
+from kb_package import tools
+
+
+class Where(ast.NodeTransformer):
+    FUNC_EQ = {
+        "lower": "$toLower",
+        "upper": "$toUpper"
+    }
+    MONGO_WHERE_CLAUSE_EQ = {
+        ast.Eq: "$eq",
+        ast.NotEq: "$neq",
+        # ast.Not: "$not",
+        # ast.BitXor: "$nor",
+        ast.Gt: "$gt",
+        ast.Lt: "$lt",
+        ast.GtE: "$gte",
+        ast.LtE: "$lte",
+        ast.In: "$in",
+        ast.NotIn: "$nin"
+        # Is
+    }
+
+    def visit_Compare(self, node):
+        node.left = self.visit(node.left)
+        comparators = []
+        for val in node.comparators:
+            val = self.visit(val)
+            comparators.append(val)
+        node.comparators = comparators
+
+        if isinstance(node.ops[0], (ast.Is, ast.IsNot)):
+            if isinstance(node.comparators[0], ast.Tuple):
+                # need to be 2 size -> (between _min and _max)
+                _min, _max = node.comparators[0].elts
+
+                result = ast.BoolOp(ast.And(), [
+                    ast.Compare(node.left, [ast.GtE()], [_min]),
+                    ast.Compare(node.left, [ast.LtE()], [_max])
+                ])
+
+                if isinstance(node.ops[0], ast.IsNot):
+                    result = ast.UnaryOp(ast.Not(), result)
+                result = self.visit(result)
+                return ast.copy_location(result, node)
+            elif isinstance(node.comparators[0], ast.Str):
+                # field is "ksksjjsjsj" -> field like "djdjdj"
+                result = ast.Call(ast.Name("like", ast.Load()),
+                                  [node.left,
+                                   node.comparators[0],
+                                   ast.Constant(isinstance(node.ops[0], ast.IsNot))
+                                   ], []
+                                  )
+                return ast.copy_location(result, node)
+            else:
+                if node.comparators[0].id.lower() in ("null", "none"):
+                    result = ast.Compare(node.left,
+                                         [ast.Eq() if isinstance(node.ops[0], ast.IsNot) else ast.NotEq()],
+                                         [node.left])
+
+                    return ast.copy_location(result, node)
+
+        if isinstance(node.ops[0], (ast.In, ast.NotIn)):
+            # print(ast.dump(node, indent=4))
+            result = ast.Call(ast.Name("in_func", ast.Load()),
+                              [node.left,
+                               node.comparators[0],
+                               ast.Constant(isinstance(node.ops[0], ast.NotIn))
+                               ], []
+                              )
+            return ast.copy_location(result, node)
+        return node
+
+    @staticmethod
+    def parse_where_clause_to_mongo(query):
+        query, quoted_text_dict = tools.replace_quoted_text(query)
+        # replace = by ==
+        query = re.sub(r"(?<!=)=(?!=)", "==", query, flags=re.I)
+        # replace <> by !=
+        query = query.replace("<>", "!=")
+
+        # extra -> like and not like
+        query = re.sub(r"\s+not\s+like\s+", " is not ", query, flags=re.I)
+        query = re.sub(r"\s+like\s+", " is ", query, flags=re.I)
+        # between
+        query = re.sub(r"\s+(not\s+)?between\s+(\w+)\s+and\s+(\w+)", r" is \1 (\2, \3) ", query, flags=re.I)
+
+        # keyword case
+        for keyword in ["in", "is", "and", "or", "not"]:
+            query = re.sub(keyword, keyword, query, flags=re.I)
+        # consider the query where not hard
+        old_query = query
+
+        # for hard queries -- modifying of query
+        # query = re.sub(r"\s+or\s+", " | ", query, flags=re.I)
+        # query = re.sub(r"\s+and\s+", " & ", query, flags=re.I)
+
+        for k in quoted_text_dict:
+            query = query.replace(k, quoted_text_dict[k])
+            old_query = old_query.replace(k, quoted_text_dict[k])
+        node = ast.parse(query)
+        if not all([isinstance(n, ast.Expr) for n in node.body]):
+            raise ValueError("Bad value given")
+        # node = Where().visit(node)
+        print(ast.unparse(node))
+        print(ast.dump(node, indent=4))
+        # {'HHH': {'$gte': 3}, '$and': [{'HP': 3, '$or': [{'H': 3}]}],
+        # '$or': [{'hpp': 3}, {'$or': [{"a='HHH' or! y": '22'}]}]}
+        return Where.get_mongo_where_clause_from_node(node.body[0].value)
+
+    @staticmethod
+    def get_mongo_where_clause_from_node(node):
+
+        if isinstance(node, ast.BoolOp):
+            result = {}
+            op = ""
+            if isinstance(node.op, ast.And):
+                op = "$and"
+            elif isinstance(node.op, ast.Or):
+                op = "$or"
+            result[op] = []
+            for nn in node.values:
+                result[op].append(Where.get_mongo_where_clause_from_node(nn))
+            return result
+        if isinstance(node, ast.Compare):
+            if isinstance(node.ops[0], (ast.Is, ast.IsNot)):
+                # consider: like, between, is null
+                if isinstance(node.comparators[0], ast.Tuple):
+                    # between
+                    _min, _max = node.comparators[0].elts
+                    return {
+                        Where.get_node_value(node.left, add=""): {
+                            "$lte": _max.value, "$gte": _min.value
+                        }
+                    }
+                elif isinstance(node.comparators[0], ast.Str):
+                    return {
+                        Where.get_node_value(node.left, add=""): {
+                            "$regex": node.comparators[0].value
+                        }
+                    }
+                else:
+                    if node.comparators[0].id.lower() in ("null", "none"):
+                        return {
+                            Where.get_node_value(node.left, add=""): None
+                        }
+                    raise ValueError("Bad where clause gotten")
+            else:
+                op = node.ops[0]
+                return {
+                    Where.get_node_value(node.left, add=""): {
+                        Where.MONGO_WHERE_CLAUSE_EQ.get(op.__class__): Where.get_node_value(node.comparators[0])
+                    }
+                }
+        if isinstance(node, ast.Call):
+            if node.func.id.lower() in Where.FUNC_EQ:
+                pass
+        if isinstance(node, ast.UnaryOp):
+            # unaryOp are (not ~)
+            if isinstance(node.op, ast.Not):
+                return {"$not": Where.get_mongo_where_clause_from_node(node.operand)}
+        if isinstance(node, ast.Name):
+            # return bool(node)
+            pass
+        return {}
+
+    @staticmethod
+    def get_node_value(node, add="$"):
+        if isinstance(node, ast.Constant):
+            right = node.value
+        elif isinstance(node, ast.Name):
+            right = add + node.id
+        elif isinstance(node, ast.Call):
+            right = ""
+        else:
+            right = ""
+        return right
+
+
+if __name__ == '__main__':
+    print(Where.parse_where_clause_to_mongo("not (b=3 or a between 100 and 300) and c >0"))
--- kb_package/database/mongodb.py
+++ kb_package/database/mongodb.py
@@ -14,16 +14,18 @@ import pymongo
 from kb_package.database.where_clause import WhereClause
 from kb_package.database.basedb import BaseDB
 from kb_package.tools import INFINITE
+from kb_package import tools
 
 
 class MongoDB(BaseDB):
     SQL_TYPE_ACCEPTED = ["SELECT", "INSERT", "DELETE", "UPDATE"]
-    SQL_SELECT_REGEX = r"^SELECT\s+(.*?)\s+" \
-                       r"FROM\s+(.*?)\s+" \
-                       r"(?:WHERE\s+(.*?))?" \
-                       r"(?:GROUP\s+BY\s+(.*?))?" \
-                       r"(?:ORDER\s+BY\s+(.*?))?" \
-                       r"(?:LIMIT\s+(.*?))?$"
+    SQL_SELECT_REGEX = r"^SELECT\s+(?P<FIELDS>.*?)\s+" \
+                       r"FROM\s+(?P<TABLE>.*?)\s+" \
+                       r"(?:WHERE\s+(?P<WHERE_CLAUSE>.*?))?" \
+                       r"(?:GROUP\s+BY\s+(?P<GROUP_BY>.*?))?" \
+                       r"(?:HAVING\s+(?P<HAVING>.*?))?" \
+                       r"(?:ORDER\s+BY\s+(?P<ORDER_BY>.*?))?" \
+                       r"(?:LIMIT\s+(?P<LIMIT>.*?))?$"
 
     SQL_AGGREGATE_REGEX = r"^(AVG|SUM|COUNT|MAX|MIN)\((.*?)\)" \
                           r"(?:(?:\sas)?\s(\w+))?$"
@@ -32,132 +34,6 @@ class MongoDB(BaseDB):
     @property
     def _get_name(self):
         return self.__class__.__name__
-
-    @staticmethod
-    def eval_sql_type(sql_code, regex):
-        res = re.match(regex, sql_code,
-                       flags=re.I | re.S)
-        if res:
-            return res.groups()
-        return res
-
-    def run_script_module(self, module, function_name, params=None):
-        """
-
-        Args:
-            module: str, specify the module you want to run
-            function_name: str, the name of request function
-            params: object, the giving params
-
-        Returns: data, the results of the requests
-        """
-        try:
-            self.reload_connexion()
-            module = importlib.import_module(module)
-            return getattr(module, function_name)(self.db_object, params)
-        except (Exception, ImportError):
-            print(traceback.format_exc())
-
-    @staticmethod
-    def fields_parser(fields: str):
-        fields = [f.strip() for f in fields.strip().split(",")]
-        if "*" in fields:
-            return None
-        final_fields = {"_id": 0}
-        aggregate_fields = {}
-        for k in fields:
-            res = re.match(MongoDB.SQL_AGGREGATE_REGEX, k, flags=re.I | re.S)
-            if res:
-                aggregate, var, alias = res.groups()
-                var = var.strip()
-                alias = (alias or k).strip()
-
-                if var == "*":
-                    var = 1
-                else:
-                    var = "$" + var
-
-                aggregate = aggregate.lower()
-                if aggregate == "count":
-                    aggregate = "sum"
-                    var = 1
-                aggregate_fields[alias] = {"$" + aggregate: var}
-            else:
-                var, alias = re.match("^(\w+?)(?:(?:\sas)?\s(\w+))?$", k,
-                                      flags=re.I | re.S).groups()
-                if alias is not None:
-                    var = {
-                        alias.strip(): "$" + var
-                    }
-                else:
-                    var = {
-                        var.strip(): 1
-                    }
-
-                final_fields.update(var)
-
-        return final_fields, aggregate_fields
-
-    @staticmethod
-    def execute_from_sql(sql_code: str, sql_type: str = None):
-        sql_code = sql_code.strip()
-        res = None
-        if sql_type is not None:
-            sql_type = sql_type.upper()
-            regex = getattr(MongoDB, "SQL_" + sql_type + "_REGEX")
-            res = MongoDB.eval_sql_type(sql_code, regex)
-        else:
-            for regex in MongoDB.SQL_TYPE_ACCEPTED:
-                res = MongoDB.eval_sql_type(
-                    sql_code, getattr(MongoDB, "SQL_" + regex + "_REGEX"))
-                if res:
-                    sql_type = regex
-                    break
-        method = None
-        args = []
-        collection = None
-        if res:
-            # print(res)
-            if sql_type == "SELECT":
-                method = "aggregate"
-                args.append([])
-                obj = args[-1]
-                fields, collection, where_clause, group_by, order_by, limit = \
-                    res
-
-                projection, aggregate_fields = MongoDB.fields_parser(fields)
-
-                where_clause = WhereClause.parse_where_clause_to_mongo(
-                    where_clause)
-                if len(where_clause):
-                    obj.append({"$match": where_clause})
-                if group_by is not None or len(aggregate_fields):
-                    if isinstance(group_by, str):
-                        group_by = {f.strip(): "$" + f.strip()
-                                    for f in group_by.split(",")}
-                        for f in group_by:
-                            if projection.get(f, 0):
-                                projection[f] = "$_id." + f
-                    else:
-                        group_by = None
-                    group_by = {"_id": group_by}
-                    group_by.update(aggregate_fields)
-
-                    obj.append({"$group": group_by})
-
-                    for key in aggregate_fields.keys():
-                        projection[key] = 1
-
-                if isinstance(projection, dict) and len(projection):
-                    obj.append({"$project": projection})
-
-                if order_by:
-                    # order_by, _ = MongoDB.fields_parser(order_by)
-                    # obj.append({"$sort": where_clause})
-                    pass
-                print(f"db.{collection}.{method}({args[0]})")
-
-        return {"collection": collection, "method": method, "args": args}
 
     # implementation of abstracts methods
     @staticmethod
@@ -239,6 +115,141 @@ class MongoDB(BaseDB):
 
     def create_table(self, arg: str, table_name=None, auto_increment_field=False, auto_increment_field_name=None):
         pass
+
+    @staticmethod
+    def eval_sql_type(sql_code, regex):
+        res = re.match(regex, sql_code,
+                       flags=re.I | re.S)
+        if res:
+            return res.groupdict()
+        return res
+
+    def run_script_module(self, module, function_name, params=None):
+        """
+
+        Args:
+            module: str, specify the module you want to run
+            function_name: str, the name of request function
+            params: object, the giving params
+
+        Returns: data, the results of the requests
+        """
+        try:
+            self.reload_connexion()
+            module = importlib.import_module(module)
+            return getattr(module, function_name)(self.db_object, params)
+        except (Exception, ImportError):
+            print(traceback.format_exc())
+
+    @staticmethod
+    def fields_parser(fields: str):
+        fields = [f.strip() for f in fields.strip().split(",")]
+        if "*" in fields:
+            # think about -> example: select *, IIF(a, 2, 3) as t from ...
+            return None
+        final_fields = {"_id": 0}
+        aggregate_fields = {}
+        for k in fields:
+            # think about using of function like iif, lower, replace, regex, <constant> ...
+            res = re.match(MongoDB.SQL_AGGREGATE_REGEX, k, flags=re.I | re.S)
+            if res:
+                aggregate, var, alias = res.groups()
+                var = var.strip()
+                alias = (alias or k).strip()
+
+                if var == "*":
+                    var = 1
+                else:
+                    var = "$" + var
+
+                aggregate = aggregate.lower()
+                if aggregate == "count":
+                    aggregate = "sum"
+                    var = 1
+                aggregate_fields[alias] = {"$" + aggregate: var}
+            else:
+                var, alias = re.match(r"^(\w+?)(?:(?:\sas)?\s(\w+))?$", k,
+                                      flags=re.I | re.S).groups()
+                if alias is not None:
+                    var = {
+                        alias.strip(): "$" + var
+                    }
+                else:
+                    var = {
+                        var.strip(): 1
+                    }
+
+                final_fields.update(var)
+
+        return final_fields, aggregate_fields
+
+    @staticmethod
+    def execute_from_sql(sql_code: str, sql_type: str = None):
+        sql_code = sql_code.strip()
+        sql_code, quoted_text_dict = tools.replace_quoted_text(sql_code)
+        res = None
+        if sql_type is not None:
+            sql_type = sql_type.upper()
+            regex = getattr(MongoDB, "SQL_" + sql_type + "_REGEX")
+            res = MongoDB.eval_sql_type(sql_code, regex)
+        else:
+            for regex in MongoDB.SQL_TYPE_ACCEPTED:
+                res = MongoDB.eval_sql_type(
+                    sql_code, getattr(MongoDB, "SQL_" + regex + "_REGEX"))
+                if res:
+                    sql_type = regex
+                    break
+        print(res)
+        method = None
+        args = []
+        collection = None
+        if res:
+            # print(res)
+            if sql_type == "SELECT":
+                method = "aggregate"
+                args.append([])
+                obj = args[-1]
+                for k in quoted_text_dict:
+                    for kk in res:
+                        res[kk] = res[kk].replace(k, quoted_text_dict[k])
+                fields, collection, where_clause, group_by, order_by, limit = \
+                    res.values()
+
+                projection, aggregate_fields = MongoDB.fields_parser(fields)
+
+                where_clause = WhereClause.parse_where_clause_to_mongo(
+                    where_clause)
+                if len(where_clause):
+                    obj.append({"$match": where_clause})
+                if group_by is not None or len(aggregate_fields):
+                    if isinstance(group_by, str):
+                        group_by = {f.strip(): "$" + f.strip()
+                                    for f in group_by.split(",")}
+                        for f in group_by:
+                            if projection.get(f, 0):
+                                projection[f] = "$_id." + f
+                    else:
+                        group_by = None
+                    group_by = {"_id": group_by}
+                    group_by.update(aggregate_fields)
+
+                    obj.append({"$group": group_by})
+
+                    for key in aggregate_fields.keys():
+                        projection[key] = 1
+
+                if isinstance(projection, dict) and len(projection):
+                    obj.append({"$project": projection})
+
+                if order_by:
+                    # order_by, _ = MongoDB.fields_parser(order_by)
+                    # obj.append({"$sort": where_clause})
+                    pass
+                print(f"db.{collection}.{method}({args[0]})")
+
+        return {"collection": collection, "method": method, "args": args}
+
+
 
 
 if __name__ == "__main__":
