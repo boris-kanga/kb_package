fb34de09b174e7ad6bb4e91c86423ca579c1523b
boris-kanga <kangaborisparfait@gmail.com>
2023-02-15 11:40:06 +0000

    FIX: error with __add__ method

--- kb_package/utils/fdataset.py
+++ kb_package/utils/fdataset.py
@@ -23,24 +23,73 @@ from pandas.core.dtypes.common import is_numeric_dtype, is_object_dtype
 import kb_package.tools as tools
 import kb_package.utils._query_func as query_func
 
+from kb_package.logger import CustomLogger
+
+Logger = CustomLogger("DatasetFactory")
+
+
+def _parse_default_col_name(item, _preserve, _ignore_case, _columns):
+    if not isinstance(item, str):
+        return item
+    if not _preserve:
+        _item = tools.format_var_name(item)
+        if _ignore_case:
+            _item = _item.upper()
+        if _item in _columns:
+            return _item
+    return item
+
+
+def _preserve_or_transform_columns(dataset, preserve, ignore_case):
+    if not preserve:
+        renamed_col = {}
+        warning_msg = []
+        for ii, col in enumerate(dataset.columns):
+            if isinstance(col, str):
+                col_ = tools.format_var_name(col, accent=False)
+                if not col:
+                    col_ = "Columns_" + str(ii)
+                if ignore_case:
+                    col_ = col_.upper()
+                renamed_col[col] = col_
+                if col.upper() != col_:
+                    warning_msg.append(repr(col) + " -> " + col_)
+        if warning_msg:
+            Logger.warning("Going to rename columns:", ",".join(warning_msg))
+
+        dataset.rename(columns=renamed_col, inplace=True)
+    return dataset
+
 
 class DatasetFactory:
     LAST_FILE_LOADING_TIME = 0
+    IGNORE_CASE = True
 
-    def __init__(self, dataset: typing.Union[pandas.DataFrame, str, list, dict] = None, **kwargs):
+    def __init__(self, dataset: typing.Union[pandas.DataFrame, str, list, dict] = None, ignore_case=None,
+                 preserve=False, **kwargs):
         # cls = self.__class__
         # cls.from_file.__code__.co_varnames
         self._path = None
+        self._preserve = preserve
+        self._ignore_case = self.IGNORE_CASE if ignore_case is None else ignore_case
         if isinstance(dataset, str):
             self._path = dataset
         if dataset is None:
             self._source = pandas.DataFrame()
         elif not isinstance(dataset, pandas.DataFrame) or (hasattr(dataset, "readable") and dataset.readable()):
-            self._source = self.from_file(dataset, **kwargs).dataset
+            self._source = self.from_file(dataset, **kwargs, ignore_case=self._ignore_case, preserve=preserve).dataset
         else:
-            self._source = dataset
+            self._source = _preserve_or_transform_columns(dataset, preserve=preserve,
+                                                          ignore_case=ignore_case)
+
+    def __parse_default_col_name(self, item):
+        return _parse_default_col_name(item, _preserve=self._preserve,
+                                       _ignore_case=self._ignore_case,
+                                       _columns=self._source.columns)
 
     def __getattr__(self, item, default=None):
+        item = self.__parse_default_col_name(item)
+        # maybe methods or accessible attribute like columns
         try:
             return getattr(self.dataset, item)
         except AttributeError as ex:
@@ -49,15 +98,20 @@ class DatasetFactory:
             raise ex
 
     def __setitem__(self, key, value):
+        key = self.__parse_default_col_name(key)
+        if self._ignore_case and not self._preserve and isinstance(key, str):
+            key = key.upper()
         self.dataset.__setitem__(key, value)
 
     def __getitem__(self, item):
+        item = self.__parse_default_col_name(item)
         return self.dataset.__getitem__(item)
 
     def __len__(self):
         return self._source.shape[0]
 
     def __delitem__(self, key):
+        key = self.__parse_default_col_name(key)
         self._source.__delitem__(key)
 
     def save(self, path=None, force=False, **kwargs):
@@ -83,7 +137,7 @@ class DatasetFactory:
         return self._source
 
     @staticmethod
-    def _format_other(other, logic):
+    def _format_other(other, logic, **kwargs):
         if isinstance(logic, dict):
             source_ref = logic["source"]
             other_ref = logic["exclude"]
@@ -98,10 +152,9 @@ class DatasetFactory:
 
         if isinstance(other, (list, pandas.Series)):
             other = pandas.DataFrame({other_ref: other})
-        elif isinstance(other, str):
-            other = DatasetFactory.from_file(other).dataset
         elif isinstance(other, DatasetFactory):
             other = other.dataset
+        other = DatasetFactory(other, **kwargs).dataset
         if not isinstance(other, pandas.DataFrame):
             raise ValueError("Bad value of other given: %s" % other)
         return other, source_ref, other_ref
@@ -116,9 +169,22 @@ class DatasetFactory:
                 other: typing.Union[list, pandas.Series, pandas.DataFrame, str],
                 exclusion_logic: typing.Union[dict, list, tuple, str],
                 op="both", columns=None, suffixes=None):
-
-        other, source_ref, other_ref = self._format_other(other, exclusion_logic)
+        kwargs = {"ignore_case": self._ignore_case, "preserve": self._preserve}
+        other, source_ref, other_ref = self._format_other(other, exclusion_logic, **kwargs)
         dataset = self.dataset.copy(deep=True)
+        if isinstance(source_ref, str):
+            source_ref = _parse_default_col_name(source_ref, _preserve=self._preserve,
+                                                 _ignore_case=self._ignore_case,
+                                                 _columns=self._source.columns)
+        else:
+            Logger.warning("use DataFrame merge method simply")
+        if isinstance(other_ref, str):
+            other_ref = _parse_default_col_name(source_ref, _preserve=self._preserve,
+                                                _ignore_case=self._ignore_case,
+                                                _columns=other.columns)
+        else:
+            Logger.warning("use DataFrame merge method simply")
+
         if is_numeric_dtype(dataset[source_ref]) and is_object_dtype(other[other_ref]):
             dataset[source_ref] = dataset[source_ref].apply(str)
         elif is_numeric_dtype(other[other_ref]) and is_object_dtype(dataset[source_ref]):
@@ -160,8 +226,14 @@ class DatasetFactory:
     @classmethod
     def from_file(cls, file_path, sep=None, drop_duplicates=False, drop_duplicates_on=None, columns=None,
                   force_encoding=True, **kwargs):
+
         start_time = time.time()
         delimiters = kwargs.pop("delimiters", [',', '\t', ';', ' ', ':'])
+        ignore_case = kwargs.pop("ignore_case", False)
+        preserve = kwargs.pop("preserve", True)
+        if "header" in kwargs and isinstance(kwargs["header"], bool):
+            kwargs["header"] = None if not kwargs["header"] else "infer"
+
         if isinstance(file_path, str):
             try:
                 is_hidden = bool(os.stat(file_path).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
@@ -171,52 +243,52 @@ class DatasetFactory:
             if is_hidden:
                 dataset = pandas.DataFrame()
             elif os.path.splitext(file_path)[1][1:].lower() in ["xls", "xlsx", "xlsm", "xlsb"]:
-                kwargs = {
+                kwargs_ = {
                     k: v for k, v in kwargs.items()
                     if k in pandas.read_excel.__wrapped__.__code__.co_varnames
                 }
-                dataset = pandas.read_excel(file_path, **kwargs)
+                dataset = pandas.read_excel(file_path, **kwargs_)
             else:
-                kwargs = {
+                kwargs_ = {
                     k: v for k, v in kwargs.items()
                     if k in pandas.read_csv.__wrapped__.__code__.co_varnames
                 }
                 if "encoding" not in kwargs:
-                    kwargs["encoding"] = "utf-8"
+                    kwargs_["encoding"] = "utf-8"
                 used_sniffer = False
                 try:
                     if sep is None:
-                        with open(file_path, encoding=kwargs["encoding"]) as file:
+                        with open(file_path, encoding=kwargs_["encoding"]) as file:
                             sample = [file.readline() for _ in range(10)]
                             try:
                                 sep = DatasetFactory._check_delimiter(sample, delimiters)
                                 assert sep, ""
-                                kwargs["sep"] = sep
+                                kwargs_["sep"] = sep
                                 used_sniffer = True
                             except (csv.Error, AssertionError):
                                 pass
                     else:
-                        kwargs["sep"] = sep
-                    dataset = pandas.read_csv(file_path, **kwargs)
+                        kwargs_["sep"] = sep
+                    dataset = pandas.read_csv(file_path, **kwargs_)
                 except UnicodeDecodeError as exc:
                     if not force_encoding:
                         raise exc
                     with open(file_path, "rb") as file_from_file_path:
                         file_bytes = file_from_file_path.read()
                         encoding_proba = chardet.detect(file_bytes).get("encoding", "latin1")
-                    if kwargs["encoding"] != encoding_proba:
-                        print("We force encoding to:", encoding_proba)
-                        kwargs["encoding"] = encoding_proba
-                        if "sep" not in kwargs or used_sniffer:
-                            with open(file_path, encoding=kwargs["encoding"]) as file:
+                    if kwargs_["encoding"] != encoding_proba:
+                        Logger.warning("We force encoding to:", encoding_proba)
+                        kwargs_["encoding"] = encoding_proba
+                        if "sep" not in kwargs_ or used_sniffer:
+                            with open(file_path, encoding=kwargs_["encoding"]) as file:
                                 sample = [file.readline() for _ in range(10)]
                                 try:
                                     sep = DatasetFactory._check_delimiter(sample, delimiters)
                                     assert sep, ""
-                                    kwargs["sep"] = sep
+                                    kwargs_["sep"] = sep
                                 except (csv.Error, AssertionError):
                                     pass
-                        dataset = pandas.read_csv(file_path, **kwargs)
+                        dataset = pandas.read_csv(file_path, **kwargs_)
                     else:
                         raise exc
         elif hasattr(file_path, "readable") and file_path.readable():
@@ -242,32 +314,63 @@ class DatasetFactory:
                                        **{k: v for k, v in kwargs.items() if k in ["index", "dtype"]})
 
         cls.LAST_FILE_LOADING_TIME = time.time() - start_time
+        dataset = _preserve_or_transform_columns(dataset, preserve=preserve,
+                                                 ignore_case=ignore_case)
 
-        if columns is not None:
-            if isinstance(columns, list):
-                dataset = dataset.loc[:, columns]
-            elif isinstance(columns, dict):
-                dataset = dataset.loc[:, columns.keys()]
-                dataset.rename(columns=columns, inplace=True)
-        if drop_duplicates:
+        if tools.BasicTypes.is_iterable(columns):
+            final_col = {}
+            dataset_columns = list(dataset.columns)
+            for ii, k in enumerate(columns):
+                alias = None
+                if isinstance(k, dict):
+                    k, alias = next(iter(k.items()))
+                if k in dataset_columns:
+                    key = k
+                    alias = columns[k] if isinstance(columns, dict) else alias or k
+                elif isinstance(k, str) and ii < len(dataset_columns):
+                    key = dataset_columns[ii]
+                    alias = columns[k] if isinstance(columns, dict) else alias or k
+                elif isinstance(k, int):
+                    key = dataset_columns[k]
+                    alias = columns[k] if isinstance(columns, dict) else alias or key
+                else:
+                    raise ValueError("Bad column %s given" % k)
+                final_col[key] = alias
+            dataset = dataset.loc[:, final_col.keys()]
+            dataset.rename(columns=final_col, inplace=True)
+
+        if drop_duplicates or kwargs.get("rd") or kwargs.get("dd"):
+            # rd -> remove duplicated, dd -> delete duplicated
+            if drop_duplicates_on is None:
+                drop_duplicates_on = kwargs.get("r_on") or kwargs.get("d_on")
             if drop_duplicates_on is None:
                 drop_duplicates_on = dataset.columns
             else:
                 drop_duplicates_on = ([drop_duplicates_on]
                                       if isinstance(drop_duplicates_on, str)
                                       else drop_duplicates_on)
+                drop_duplicates_on = [_parse_default_col_name(d, _preserve=preserve,
+                                                              _ignore_case=ignore_case,
+                                                              _columns=dataset.columns)
+                                      for d in drop_duplicates_on]
                 drop_duplicates_on = dataset.columns.intersection(drop_duplicates_on)
             if drop_duplicates_on.shape[0]:
                 dataset.drop_duplicates(inplace=True,
                                         subset=drop_duplicates_on, ignore_index=True)
         return cls(dataset)
 
+    def doublon(self, drop=False):
+        pass
+
     def __add__(self, other):
         if isinstance(other, self.__class__):
             other = other.dataset
         if self._source.empty or (
                 len(self._source.columns) == len(other.columns) and all(self._source.columns == other.columns)):
-            return DatasetFactory(pandas.concat([self._source, other], ignore_index=True, sort=False))
+
+            return DatasetFactory(pandas.concat([self._source, other], ignore_index=True, sort=False),
+                                  preserve=self._preserve,
+                                  ignore_case=self._ignore_case)
         return self._source.__add__(other)
 
     __radd__ = __add__
@@ -286,7 +389,8 @@ class DatasetFactory:
                 if callable(v):
                     permit_funcs.extend(str(k).lower())
                     params[str(k).lower()] = v
-            hard, query = QueryTransformer(permit_funcs=permit_funcs).process(query)
+            hard, query = QueryTransformer(permit_funcs=permit_funcs, ignore_case=self._ignore_case
+                                           ).process(query)
             if hard:
                 res = tools.safe_eval_math(query, params=params, dataset=self._source, method="exec", **kwargs)
                 if kwargs.get("inplace"):
@@ -319,14 +423,23 @@ class DatasetFactory:
             if callable(v):
                 permit_funcs.extend(str(k).lower())
                 params[str(k).lower()] = v
+
         query = (
-                "\ndef apply(serie):\n"
-                "\tres = " + QueryTransformer("serie", hard=True, permit_funcs=permit_funcs).process(func, _for="apply") + "\n"
-                "\treturn res.item() if hasattr(res, 'ndim') and res.ndim==0 else res\n"
-                "result = dataset.apply(apply, axis=1) \n"
+            "\ndef apply(serie):\n"
+            "\tres = "
+        )
+        query += QueryTransformer("serie", hard=True, permit_funcs=permit_funcs,
+                                  ignore_case=self._ignore_case).process(func,
+                                                                         _for="apply")
+        query += (
+            "\n"
+            "\treturn "
+            "res.item() if hasattr(res, 'ndim') and "
+            "res.ndim==0 else res\n"
+            "result = dataset.apply(apply, axis=1) \n"
         )
 
-        print("Going to run", query)
+        Logger.info("Going to run", query)
 
         return tools.safe_eval_math(query, params=params, dataset=self._source, method="exec", **kwargs)
 
@@ -348,7 +461,7 @@ class QueryTransformer(ast.NodeTransformer):
                     isinstance(m, numpy.vectorize))))
     }
 
-    def __init__(self, *args, hard=False, permit_funcs=None):
+    def __init__(self, *args, hard=False, permit_funcs=None, ignore_case=True):
         super().__init__()
         if len(args):
             self.PREFIX = args[0]
@@ -360,6 +473,7 @@ class QueryTransformer(ast.NodeTransformer):
         self._custom_permit_func.extend(list(self.PERMIT_FUNC.keys()))
         self._hard = hard
         self._use_attr = False
+        self._ignore_case = ignore_case
         self.list_name = []
 
     def visit_Attribute(self, node):
@@ -369,6 +483,8 @@ class QueryTransformer(ast.NodeTransformer):
         # print("name", ast.dump(node, indent=2))
         if node.id.lower() not in ("null", "none"):
             self.list_name.append(node)
+            if self._ignore_case:
+                self._hard = True
         return node
 
     def visit_BoolOp(self, node):
@@ -507,6 +623,8 @@ class QueryTransformer(ast.NodeTransformer):
                 raise NotImplementedError("Not permit to got attribute in this mode")
             for name in list_name:
                 if not name.id.startswith(self.PREFIX):
+                    if self._ignore_case:
+                        name.id = name.id.upper()
                     name.id = self.PREFIX + name.id
             res = ast.unparse(tree)
             if _for != "query":
@@ -516,12 +634,14 @@ class QueryTransformer(ast.NodeTransformer):
             #    pycode = py.read()
             final_query = "result = " + self.PREFIX[:-1] + "[" + res + "]"
             pycode += final_query
-            if verbose or True:
-                print("Got final script -->", self.PREFIX[:-1] + "[" + res + "]")
+            if verbose:
+                Logger.info("Got final script -->", self.PREFIX[:-1] + "[" + res + "]")
             # code = compile(tree, "<string>", "exec")
             # exec(code)
         else:
             # here we consider the query is not hard so returns it
+            if verbose:
+                Logger.info("Got final script -->", old_query)
             pycode = old_query
         if _for != "query":
             return old_query
