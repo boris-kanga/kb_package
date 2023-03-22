e1a272be604f7d96a7e93ea51d35781a77c328c1
boris-kanga <kangaborisparfait@gmail.com>
2023-03-22 09:46:54 +0000

    Refact: partially import due sometimes error

--- kb_package/utils/__init__.py
+++ kb_package/utils/__init__.py
@@ -1,4 +1,7 @@
-from .fdataset import DatasetFactory
+try:
+    from .fdataset import DatasetFactory
+except (ImportError, Exception):
+    pass
 try:
     from .fexcel import ExcelFactory
 except (ImportError, Exception):
